6c28424a9f18a117779451f4a30f4869ad578c42
Jodie Glantzlen <68207139+jodie-glantzlen@users.noreply.github.com>
2022-06-17 14:52:02 +1200

    fix rick

--- client/components/Mystery.jsx
+++ client/components/Mystery.jsx
@@ -7,7 +7,7 @@ function Mystery() {
         <div className='rick-title'>
           <p>You've been RickRolled!</p>
         </div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&enable" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1&enable" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </>
   )
