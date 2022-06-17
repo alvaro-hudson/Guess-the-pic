bbd734c011b7ba2508919e9c5682513552642048
jared-sukha <jared.sukha@gmail.com>
2022-06-17 15:36:17 +1200

    fin

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <>
-      <h1>Home's page! Wooo!</h1>
+    
+    <div className='home-container'>
+      <h1>Homepage! Wooo!</h1>
       <Link to='/jodie'><h1>Jodie's page</h1></Link>
       <Link to='/jared'><h1>Jared's page</h1></Link>
       <Link to='/alvaro'><h1>Alvaro's page</h1></Link>
       <Link to='/mystery'><h1>Mystery page</h1></Link>
-    </>
+      </div>
   )
 }
 
