393c757ca9151f5a59978ac1020a93ccd0af8143
jared-sukha <jared.sukha@gmail.com>
2022-06-17 14:42:29 +1200

    inital animate

--- client/components/Jared.jsx
+++ client/components/Jared.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
+import { motion } from "framer-motion"
 
 function Pixel() {
   const clickHandler = () => {
-    setStyle({
-      height: '10vh',
-      width: '5vw',
-      opacity: 1.2
-    })
+    setDiv(false)
   }
 
   const [style, setStyle] = useState({
@@ -15,11 +12,26 @@ function Pixel() {
     backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
   })
 
+  const [normalDiv, setDiv] = useState(true)
+
   return (
     <>
-      <div style={style}
-        onClick={() => clickHandler()}>
-      </div>
+    {normalDiv ? ( 
+    <div style={style} 
+        onClick={() => clickHandler()}
+      />
+      ) :( 
+      <motion.div style={style}
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 270, 270, 0],
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        opacity: [1,1,1,1,0]
+      }}
+        onClick={() => clickHandler()}
+      />
+    )}
+      
       </>
   )
 }
